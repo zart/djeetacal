ebad88ba94a2202f98a74733e7d4e2c72d817c73
Konstantin Zemlyak <zart@zartsoft.ru>
2019-11-10 22:39:55 +0500

    Add Vue 2 (and vue-template-compiler)

--- src/app/index.js
+++ src/app/index.js
@@ -1,1 +1,13 @@
 // application entry point
+
+// import stuff
+import Vue from 'vue'
+
+// configure Vue
+Vue.config.productionTip = false
+
+// app itself
+const app = new Vue({
+  render: h => h()
+})
+app.$mount('#app')
